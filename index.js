4acee3202a549712d5317b80e9928c9aa128c553
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 22:24:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+} 
+
+function increaseRankBy(n){
+  const ranked = document.querySelectorAll(".ranked-list li")
+  for(let i = 0; i < ranked.length; i++){
+    ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var divs = document.querySelectorAll("div#grand-node div")
+  return divs[divs.length-1]
+}
